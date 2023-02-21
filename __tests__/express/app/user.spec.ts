689eceb31a2e990255001601f2f021ff333e948f
shahan <shahan.ahmed@apify.ai>
2023-02-22 01:21:09 +0500

    App test cases covered

--- __tests__/express/app/user.spec.ts
+++ __tests__/express/app/user.spec.ts
@@ -2,20 +2,35 @@ import chai from 'chai';
 import { UserArgs } from '../../../src/@types/api.type';
 import { SHOULD_OMIT_PROPS } from '../../../src/utils/constants.util';
 import { uploadImage } from '../../common/common.helper';
-import { changePassword, loggedIn, login, updateProfile } from './admin.helper';
+import { changePassword, loggedIn, login, signup, updateProfile } from './user.helper';
 
 const { expect } = chai;
 
-const UPDATE_PROFILE_DATA: UserArgs = {
-	avatar: 'temp/uuid-filename.txt',
-	firstName: 'Shahan',
-	lastName: 'Ahmed Khan',
-	phone: '+923422662425',
+const SIGNUP_DATA: UserArgs = {
+	firstName: 'Shahan Ahmed',
+	lastName: 'Khan',
+	phone: '+923362122588',
 	genderId: '04521c7b-a128-4f5f-bfb2-96053c0a31b0',
 };
 
-describe('RESTful - Admin Authentication APIs', function () {
-	it('admin login', async () => {
+describe('RESTful - App Authentication APIs', function () {
+	it('app signup', async () => {
+		const { body: uploadedFile } = await uploadImage();
+
+		const payload = Object.assign<UserArgs, UserArgs, UserArgs>({}, SIGNUP_DATA, {
+			avatar: uploadedFile.data,
+			email: 'shahan.khaan@gmail.com',
+			password: '123Abc456',
+		});
+		const res = await signup(payload); // should success
+		expect(res.error).to.be.false;
+		expect(res.status).to.be.equal(201);
+		expect(res.body.data.token).to.be.a('string');
+		['token', 'user'].map((prop) => expect(res.body.data).to.have.property(prop));
+		SHOULD_OMIT_PROPS.map((prop) => expect(res.body.data.user).not.to.have.property(prop));
+	});
+
+	it('app login', async () => {
 		let res = await login('shahan', 'shahan'); // should fail
 		expect(res.error).not.to.be.false;
 		expect(res.status).to.be.equal(401);
@@ -28,22 +43,26 @@ describe('RESTful - Admin Authentication APIs', function () {
 		SHOULD_OMIT_PROPS.map((prop) => expect(res.body.data.user).not.to.have.property(prop));
 	});
 
-	it('admin loggedIn', async () => {
+	it('app loggedIn', async () => {
 		const { body } = await login();
 		const token = body.data.token;
+		console.log('token...', token);
 
 		const res = await loggedIn(token);
+		console.log('res.error', res.error);
+		console.log('res.status', res.status);
 		expect(res.error).to.be.false;
 		expect(res.status).to.be.equal(200);
 		SHOULD_OMIT_PROPS.map((prop) => expect(res.body).not.to.have.property(prop));
 	});
 
-	it('admin updateProfile', async () => {
+	it('app updateProfile', async () => {
 		const { body } = await login();
 		const token = body.data.token;
 
 		const { body: uploadedFile } = await uploadImage();
-		const payload = Object.assign<UserArgs, UserArgs, UserArgs>({}, UPDATE_PROFILE_DATA, {
+
+		const payload = Object.assign<UserArgs, UserArgs, UserArgs>({}, SIGNUP_DATA, {
 			avatar: uploadedFile.data,
 		});
 
@@ -52,7 +71,7 @@ describe('RESTful - Admin Authentication APIs', function () {
 		expect(res.status).to.be.equal(201);
 	});
 
-	it('admin changePassword', async () => {
+	it('app changePassword', async () => {
 		const { body } = await login();
 		const token = body.data.token;
 
