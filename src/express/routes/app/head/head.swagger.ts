b3899bd6f5b3f91220aec06eb02bb79d1d530889
shahan <shahan.khaan@gmail.com>
2023-06-21 15:03:20 +0500

    docs: update docs of project, and swagger

--- src/express/routes/app/head/head.swagger.ts
+++ src/express/routes/app/head/head.swagger.ts
@@ -6,23 +6,69 @@
 
 /**
  * @openapi
- * /api/app/head?skip=1&take=10:
+ * /api/app/head:
  *   get:
- *     summary: Get Heads
+ *     summary: Get Heads without parent ID
  *     tags: [Head]
  *     parameters:
  *       - in: query
- *         name: skip
+ *         name: offset
  *         schema:
+ *           required: true
  *           type: integer
  *           default: 1
  *         description: The number of items to skip before starting to collect the result set
  *       - in: query
- *         name: take
+ *         name: limit
  *         schema:
+ *           required: true
  *           type: integer
  *           default: 10
  *         description: The numbers of items to return
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           required: false
+ *           type: string
+ *         description: The option to search inside the result
+ *     responses:
+ *       200:
+ *         description: Returns list of heads
+ */
+
+/**
+ * @openapi
+ * /api/app/head/{parentId}:
+ *   get:
+ *     summary: Get Heads with parent ID
+ *     tags: [Head]
+ *     parameters:
+ *       - in: query
+ *         name: offset
+ *         schema:
+ *           required: true
+ *           type: integer
+ *           default: 1
+ *         description: The number of items to skip before starting to collect the result set
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           required: true
+ *           type: integer
+ *           default: 10
+ *         description: The numbers of items to return
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           required: false
+ *           type: string
+ *         description: The option to search inside the result
+ *       - in: path
+ *         name: parentId
+ *         schema:
+ *           required: true
+ *           type: string
+ *         description: The parent ID of the entity
  *     responses:
  *       200:
  *         description: Returns list of heads
