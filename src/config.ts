553edcd8d5a533bb7c640d16765775413c2d6e80
shahan <shahan.khaan@gmail.com>
2023-09-10 23:18:38 +0500

    chore: updating envs and config file

--- src/config.ts
+++ src/config.ts
@@ -7,40 +7,63 @@ export const Configs = z.object({
 		env: z.enum(['development', 'test', 'production']).default('development'),
 		protocol: z.enum(['http:', 'https:']).default('http:'),
 		host: z.string().default('localhost:7000'),
-		port: z.string().length(4).default('7000'),
+		port: z
+			.string()
+			.length(4)
+			.transform((arg) => parseInt(arg, 10))
+			.default('7000'),
 		inProd: z.boolean().default(false),
 		baseUrl: z.string().url('Invalid Base URL').default('http://localhost:7000'),
 	}),
 	jwt: z.object({
-		access: z.object({
-			secret: z.string().min(1).default('jwt-access-secret'),
-			expiry: z.string().min(1).default('3600000'),
-		}),
-		refresh: z.object({
-			secret: z.string().min(1).default('jwt-refresh-secret'),
-			expiry: z.string().min(1).default('3600000'),
-		}),
+		secret: z.string().min(1).default('jwt-secret'),
+		expiry: z
+			.string()
+			.min(1)
+			.transform((arg) => parseInt(arg, 10))
+			.default('300000'),
 	}),
 	db: z.object({
 		host: z.string().min(1).default('db'),
-		port: z.string().length(4).default('5432'),
+		port: z
+			.string()
+			.length(4)
+			.transform((arg) => parseInt(arg, 10))
+			.default('5432'),
 		username: z.string().min(1).default('admin'),
 		password: z.string().min(1).default('lmelg8'),
 		database: z.string().min(1).default('dev_database'),
 		logging: z.boolean().default(false),
 	}),
 	bcrypt: z.object({
-		salt: z.string().min(2).default('10'),
-		maxBytes: z.string().min(2).default('72'),
+		salt: z
+			.string()
+			.min(2)
+			.transform((arg) => parseInt(arg, 10))
+			.default('10'),
+		maxBytes: z
+			.string()
+			.min(2)
+			.transform((arg) => parseInt(arg, 10))
+			.default('72'),
 	}),
 	redis: z.object({
 		host: z.string().min(1).default('cache'),
-		port: z.string().length(4).default('6379'),
+		port: z
+			.string()
+			.length(4)
+			.transform((arg) => parseInt(arg, 10))
+			.default('6379'),
 		pass: z.string().min(1).default('lmelg8'),
 		url: z.string().min(1).default('redis://cache:6379'),
 	}),
 	session: z.object({
 		secret: z.string().min(1).default('session-secret'),
+		expiry: z
+			.string()
+			.min(1)
+			.transform((arg) => parseInt(arg, 10))
+			.default('300000'),
 	}),
 	cron: z.object({
 		removeTempAt: z.string().min(9).default('15 10 * * *'),
@@ -56,17 +79,11 @@ const allEnvs = {
 		host: process.env.APP_HOST,
 		port: process.env.APP_PORT,
 		inProd: process.env.NODE_ENV === 'production',
-		// baseUrl: `${process.env.APP_PROTOCOL}//${process.env.APP_HOST}`,
+		baseUrl: `${process.env.APP_PROTOCOL}//${process.env.APP_HOST}`,
 	},
 	jwt: {
-		access: {
-			secret: process.env.JWT_ACCESS,
-			expiry: process.env.JWT_ACCESS_LIFE,
-		},
-		refresh: {
-			secret: process.env.JWT_REFRESH,
-			expiry: process.env.JWT_REFRESH_LIFE,
-		},
+		secret: process.env.JWT_SECRET,
+		expiry: process.env.SESSION_LIFE,
 	},
 	db: {
 		host: process.env.DB_HOST,
@@ -88,6 +105,7 @@ const allEnvs = {
 	},
 	session: {
 		secret: process.env.SESSION_SECRET,
+		expiry: process.env.SESSION_LIFE,
 	},
 	cron: {
 		removeTempAt: process.env.REOMOVE_TEMP_AT,
