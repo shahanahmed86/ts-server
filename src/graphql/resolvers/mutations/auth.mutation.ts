65c579a78ca8d454d6ed08ecfd1724bea4780d89
shahan <shahan.khaan@gmail.com>
2023-09-11 18:13:52 +0500

    feat: added graphql logout endpoints

--- src/graphql/resolvers/mutations/auth.mutation.ts
+++ src/graphql/resolvers/mutations/auth.mutation.ts
@@ -5,12 +5,14 @@ const auth = {
 	// admin
 	adminChangePassword: graphqlWrapper(controllers.admin.changePassword),
 	adminLogin: graphqlWrapper(controllers.admin.login),
+	adminLogout: graphqlWrapper(controllers.admin.logout),
 
 	// user
 	userUpdateProfile: graphqlWrapper(controllers.user.updateProfile),
 	userChangePassword: graphqlWrapper(controllers.user.changePassword),
 	userLogin: graphqlWrapper(controllers.user.login),
 	userSignup: graphqlWrapper(controllers.user.signup),
+	userLogout: graphqlWrapper(controllers.user.logout),
 };
 
 export default auth;
